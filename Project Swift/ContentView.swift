88886a50d219879ba13821dcc472ce50822f4898
Complete <96483406+completez@users.noreply.github.com>
2024-06-16 01:49:14 -0700

    Update

---
+++ Project Swift/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Project Swift
+//
+//  Created by Admin on 15/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var selectionFirst:String = ""
+    @State var selectionTwo:String = ""
+    @State var selectPriceTime:String = ""
+    let data =
+    ["สยาม","สามย่าน","ดอนเมือง","สุวรรณภูมิ"]
+    
+    var body: some View {
+        HStack {
+            Text("Wanderler Guide 🤯 ")
+                .font(.largeTitle)
+            Spacer()
+        }.padding()
+        HStack{
+            Text("เลือกต้นทาง")
+                .font(.headline)
+            Picker("Test",selection: $selectionFirst) {
+                ForEach(data , id: \.self) {
+                    Text($0)
+                }
+            }
+            
+            Spacer()
+            VStack{
+                Text("ราคา / เวลา")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Picker("ราคา / เวลา", selection: $selectPriceTime) {
+                ForEach(sortedData, id: \.id) { route in
+                    Text("\(route.price) บาท / \(route.time) นาที")
+                }
+            }
+            .pickerStyle(MenuPickerStyle()) // You can change the picker style as needed
+            
+            }
+        }.padding()
+        HStack{
+            Text("เลือกปลายทาง")
+                .font(.headline)
+            Picker("Test",selection: $selectionTwo) {
+                ForEach(data , id: \.self) {
+                    Text($0)
+                        
+                }
+            }
+            Spacer()
+            
+        }.padding()
+        Spacer()
+        
+        if (selectionTwo.isEmpty || selectionFirst.isEmpty) {
+            Text("กรุณาเลือกเส้นทาง ❗️")
+                .font(.title)
+        }
+        
+        ForEach (sortedData) {
+            dataAll in
+            if (!selectionTwo.isEmpty && !selectionFirst.isEmpty) && (selectionFirst != selectionTwo) && (dataAll.name1 == selectionFirst && dataAll.name2 == selectionTwo){
+                Text("\(selectionFirst) - \(selectionTwo) ใช้ระยะเวลา \(dataAll.time) นาที ราคา \(dataAll.price) ✅")
+
+                Spacer()
+                    .frame(height: 30)
+            }
+        }.padding()
+       
+        
+        Spacer()
+    }
+}
+
+#Preview {
+    ContentView()
+}
